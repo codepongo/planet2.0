6bbca795e803ad0f80c30eba0d4c9d97087add35
zhuhuotui@gmail.com <zhuhuotui@gmail.com>
2013-08-27 08:22:21 +0400

    backup

--- planet.sh
+++ planet.sh
@@ -1,3 +1,4 @@
 #/bin/sh
+cp /home/zuohaitao/www/html/rss/index.html /home/zuohaitao/www/html/rss/index`date '+%Y%m%d'`.html
 cd /home/zuohaitao/planet
 python planet.py config.ini > ../log/planet.log
