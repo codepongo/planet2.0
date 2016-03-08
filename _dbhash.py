1aa018965b91d4899bdba0d977e1f972a0044927
zuohaitao <zhuhuotui@gmail.com>
2016-03-08 22:38:44 +0800

    fix bug

--- _dbhash.py
+++ _dbhash.py
@@ -8,10 +8,14 @@ class _dbm(object):
         return self.dbm.keys()
     def sync(self):
         pass
+    def has_key(self, k):
+        return self.dbm.has_key(k)
     def __getitem__(self, k):
         return self.dbm[k]
     def __setitem__(self, k, v):
         self.dbm[k] = v
+    def __delitem__(self, k):
+        del(self.dbm[k])
 def open(filename, flag, mode):
     return _dbm(dbm.open(filename, flag, mode))
 
